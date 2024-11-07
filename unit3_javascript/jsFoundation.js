ec87d6681b97329f453083abf4b3d3e805c9b91f
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-11-07 18:40:16 +0000

    quiz.js

---
+++ unit3_javascript/jsFoundation.js
@@ -0,0 +1,30 @@
+// Data types= basic building blocks of the javascript
+// Programing language.
+
+// strings = data bound together by qoutation marks.
+" this is sentence wrapped in qoutes- this is a string"
+"1232321- this is a number BUT , when in qoutes makes a string"
+
+// Interger = numbers - doesnt matter if its a whole number
+// our a decimal
+8877
+78777
+5555
+0
+
+
+// boolean = true or false
+true , 1 
+false , 0
+
+// variables - are containers for variables
+// when writing a variable we break it down to 3 steps
+// 1. declare the type of variables we're using
+var // universal container - cant go wrong with this 
+let // temperory variable - data you intent to change
+const // constant varuiables - data that will NOTTT change
+
+let weather _by_city = "philly"
+let weather_current_temp =80
+let weather_by_day = "thursday"
+const logo= "weatherchannellogo.jpg"
