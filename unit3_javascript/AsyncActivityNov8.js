3dc1e333387c84a848eeeead6cdb3e858bde7eb0
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-11-26 18:38:37 +0000

    ExtraCredit

---
+++ unit3_javascript/AsyncActivityNov8.js
@@ -0,0 +1,75 @@
+What does it "string concatenation" mean? Please write your response as a string and also provide a code example of two variables being concatenated and output the result in your terminal.
+if you are learning JavaScript you will be using the console.log() function.
+
+'Con - cat - ten - a - tion'
+'String concatenation is when you combine 2 or more strings with the + ( addtion symbol ) operation'
+
+
+
+
+if you are learning Pyhon you will be using the print() function.
+
+Please list and describe three rules for creating variable names. Please provide your answer as a string.
+
+'const - is used to declare variables that WILL NOT CHANGE '
+'Let - is used to declare variables that WILL change'
+'var - universal /  default variable , can be used in any context.'
+
+'1. variables names cannot start with a number'
+'2. Variable names cant have any whte spaces'
+'3. Variable names cannot be reserve words/keywords'
+
+
+
+
+Define each of the following variable name formats then provide a code example of how you would write them.
+
+Camel case - every word after the fisrt word Is capitalized.
+var userName = ''
+Var timeRemainingInQrtr = ''
+
+
+//Snake case - Every word seprated by an underscore.
+var user_Name = ''
+var time_Remaining_In_Qrtr = ''
+
+
+//Pascal case - Every word Is capitalized.
+
+
+//Find the missing variable for each of the following assignment operaor code snippets
+//Python Version a = '?'
+
+b = a *100
+
+print(b) // the output in the terminal should be 300.
+
+//JavaScript Version let a = '?'
+
+let b = a *b
+
+
+print(b) // the output in the terminal should be 700.
+
+//Use the logical operators and evaluate the following variables to get the requested result.
+//Python Version
+
+//which logical operator would you use to reflect that the result between the code expressions are true. use the print function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+var x= 12
+
+var y= 2
+
+var z= 12/y
+
+consol.log(x <=12___z < 10 )
+// 12 Is less than or equal to 12 
+//JavaScript Version
+
+////hich logical operator would you use to reflect that the result between the code expressions below are false. Use the console.log() function to output the result in the termial, then explain why you would use that particular logical operator using a string.
+var x= 12
+
+var y= 2
+
+var z= 12/4
+
+x <= 12 _____ z < 10
