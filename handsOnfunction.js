1a585c0020c9a6f10a721720e259a6920a7083e6
AGary9 <158070552+AGary9@users.noreply.github.com>
2024-12-09 17:46:32 +0000

    asynchmakeupwork.py

---
+++ handsOnfunction.js
@@ -0,0 +1,13 @@
+function hoursToMintes(hour) {
+    if (hour < 12) {
+        consol.log("you will get normal pay for the day");
+    } else if()}
+    
+
+    consol.log("you qaulify for over time");
+
+
+hoursToMinutes(25);
+
+
+// 
