52b85e615a5d911f13c801d153265f580a9e1248
AGary9 <158070552+AGary9@users.noreply.github.com>
2025-01-10 18:32:34 +0000

    AsynchWork.py

---
+++ Unit 4/JavaScriptLoops.js
@@ -0,0 +1,11 @@
+function educationRank(){
+    if (gradeLevel == 1) {
+        console.log(" you are a freshman in undergrad.");
+        else if (gradelevel == 2){
+            else if (gradeLevel == 3) {
+                consol.log("you are a Junior in undergrad");
+                else if ("you are a")
+            }
+        }
+    }
+}
